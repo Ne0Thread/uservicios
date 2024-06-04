41ee0c3a3fb53d77e1dcb99fd4c1ec0978a0d1a8
lizethmiele <52666474+lizethmiele@users.noreply.github.com>
2024-06-04 15:43:35 -0500

    Ya se crean las unidades de servicio

--- src/app/admin-servicios/components/form-add-serv/form-add-serv.component.ts
+++ src/app/admin-servicios/components/form-add-serv/form-add-serv.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { U_Servicio_Service } from '../../services/unidadServicio.service'; // Asegúrate de importar el servicio correcto
-import { UnidadRegistro } from '../../interfaces/unidadRegistro.interface'; // Importa la interfaz correcta
+import { U_Servicio_Service } from '../../services/unidadServicio.service';
+import { UnidadRegistro } from '../../interfaces/unidadRegistro.interface';
 
 @Component({
   selector: 'app-form-add-serv',
@@ -11,7 +11,7 @@ import { UnidadRegistro } from '../../interfaces/unidadRegistro.interface'; // I
 export class FormAddServComponent {
 
   unidadForm: FormGroup;
-  token: string = 'token'; // Asegúrate de obtener y almacenar el token JWT correctamente
+  token: string = 'token';
 
   constructor(private serv: U_Servicio_Service, private fb: FormBuilder) {
     this.unidadForm = this.fb.group({
@@ -34,45 +34,35 @@ export class FormAddServComponent {
     return this.unidadForm.get('horarioFin');
   }
 
-  // Función para convertir el valor del input de hora a cadena
-  convertTimeToString(time: string): string {
-    // Puedes ajustar el formato según tus necesidades, aquí simplemente se retorna el valor tal cual
-    return time;
+  formatTime(time: string): string {
+    const date = new Date();
+    const [hours, minutes] = time.split(':');
+    date.setHours(parseInt(hours, 10));
+    date.setMinutes(parseInt(minutes, 10));
+    date.setSeconds(0);
+    return date.toISOString().split('T')[1].substr(0, 8);
   }
 
   addUnidad() {
     if (this.unidadForm.valid) {
-      console.log(this.unidadForm.value);
+      console.log(this.unidadForm.value);      
 
-      // Convertir los valores de hora a cadenas
-      const horarioInicioStr = this.convertTimeToString(this.unidadForm.value.horarioInicio);
-      const horarioFinStr = this.convertTimeToString(this.unidadForm.value.horarioFin);
-
-      // Crear el objeto de la nueva unidad de servicio
+      // objeto de la nueva unidad de servicio
       const nuevaUnidad: UnidadRegistro = {
-        nombre: this.unidadForm.value.nombreUnidad,
-        horarioLaboralInicio: horarioInicioStr,
-        horarioLaboralFin: horarioFinStr,
+        name : this.unidadForm.value.nombreUnidad,
+        startWorkingHours: this.formatTime(this.unidadForm.value.horarioInicio),
+        endWorkingHours: this.formatTime(this.unidadForm.value.horarioFin)
       };
 
-      // Asegurarse de que el token no tiene espacios en blanco
-      const cleanedToken = this.token.trim();
-
-      // Depuración adicional
-      console.log('Token JWT:', cleanedToken);
-      console.log('Nueva Unidad:', JSON.stringify(nuevaUnidad));
-
       // Llamar al servicio para crear la unidad de servicio
-      this.serv.createUnidad(nuevaUnidad, cleanedToken).subscribe({
+      this.serv.createUnidad(nuevaUnidad, this.token).subscribe({
         next: (response) => {
           console.log('Unidad de servicio creada:', response);
           alert('Unidad Registrada con exito');
-          // Maneja la respuesta exitosa aquí, por ejemplo, mostrar un mensaje al usuario
         },
         error: (err) => {
-          console.error('Error al crear unidad de servicio:', err);
-          alert('Error al registrar unidad: ' + err.error);
-          // Maneja el error aquí, por ejemplo, mostrar un mensaje de error al usuario
+          console.error('Error al crear unidad de servicio:', JSON.stringify(err));
+          alert('Error al registrar unidad: ' + JSON.stringify(err));
         },
       });
 
